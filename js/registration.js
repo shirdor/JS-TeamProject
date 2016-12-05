4a4699df530063954e5fd040c8e484897b90cd60
Shirdor <antoniobuyukliev@yahoo.com>
2016-12-05 16:57:33 +0200

    some comits

--- js/registration.js
+++ js/registration.js
@@ -9,38 +9,7 @@ function startApp() {
     };
 
     if(sessionStorage.username){
-            const baseUrl = "https://baas.kinvey.com/appdata/kid_HJ1-7ACGx/";
-            const kinveyAppAuthHeaders = {
-                'Authorization': "Basic " +
-                btoa(sessionStorage.username + ":" + sessionStorage.pass)
-            };
-            $.ajax({
-                method: "GET",
-                url: baseUrl +"themes",
-                headers: kinveyAppAuthHeaders
-            }).then(function (result) {
-                $('#themes').find(".theme").remove();
-                for(let theme of result){
-                    let div = $('<a href="#">');
-                    div.addClass("theme");
-                    div.text(theme.title);
-                    $('#themes').append(div).append($('<br>'));
-                }
-            });
-            $.ajax({
-                method: "GET",
-                url: baseUrl + "questions",
-                headers: kinveyAppAuthHeaders,
-            }).then(function (result) {
-                $('#forumQuestions').find(".question").remove();
-                for(let question of result){
-                    let div = $('<div>');
-                    div.addClass("question");
-                    div.text(question.title);
-                    $('#forumQuestions').append(div);
-                }
-            })
-
+        getThemes();
     }
 
     showBars();
@@ -162,37 +131,8 @@ function startApp() {
         }
 
         if(sessionStorage.username){
-            const baseUrl = "https://baas.kinvey.com/appdata/kid_HJ1-7ACGx/";
-            const kinveyAppAuthHeaders = {
-                'Authorization': "Basic " +
-                btoa(sessionStorage.username + ":" + sessionStorage.pass)
-            };
-            $.ajax({
-                method: "GET",
-                url: baseUrl +"themes",
-                headers: kinveyAppAuthHeaders
-            }).then(function (result) {
-                $('#themes').find(".theme").remove();
-                for(let theme of result){
-                    let div = $('<a href="#">');
-                    div.addClass("theme");
-                    div.text(theme.title);
-                    $('#themes').append(div).append($('<br>'));
-                }
-            });
-            $.ajax({
-                method: "GET",
-                url: baseUrl + "questions",
-                headers: kinveyAppAuthHeaders,
-            }).then(function (result) {
-                $('#forumQuestions').find(".question").remove();
-                for(let question of result){
-                    let div = $('<div>');
-                    div.addClass("question");
-                    div.text(question.title);
-                    $('#forumQuestions').append(div);
-                }
-            })
+            getThemes()
+
 
         }
 
--- js/themes.js
+++ js/themes.js
@@ -1,4 +1,4 @@
-$('#themes-bar').click(function getThemes() {
+function getThemes() {
     const baseUrl = "https://baas.kinvey.com/appdata/kid_HJ1-7ACGx/";
     const kinveyAppAuthHeaders = {
         'Authorization': "Basic " +
@@ -10,8 +10,9 @@ $('#themes-bar').click(function getThemes() {
         headers: kinveyAppAuthHeaders
     }).then(function (result) {
         $('#themes').find(".theme").remove();
+        $('#themes').find("br").remove();
         for(let theme of result){
-            let div = $('<a href="#">');
+            let div = $('<a href="#themes">');
             div.addClass("theme");
             div.text(theme.title);
             $('#themes').append(div).append($('<br>'));
@@ -30,4 +31,4 @@ $('#themes-bar').click(function getThemes() {
             $('#forumQuestions').append(div);
         }
     })
-});
+}
