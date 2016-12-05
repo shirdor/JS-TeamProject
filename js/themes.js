d37286439a8681435c22a607c2597f4ea6e8ff87
WhiteGyrfalcon <konstantin.filosofov@gmail.com>
2016-12-06 00:26:49 +0200

    Listing problems

--- js/themes.js
+++ js/themes.js
@@ -30,6 +30,14 @@ function getThemes() {
         headers: kinveyAppAuthHeaders,
     }).then(function (result) {
         $('#forumQuestions').find(".question").remove();
+        let counter = 0;
+        let typeOfTheme = {
+            music:'<i class="fa fa-music fa-2x text-primary"></i></td>',
+            game:'<i class="fa fa-gamepad  fa-2x text-primary"></i>',
+            school:'<i class="fa fa-graduation-cap fa-2x text-primary"></i>',
+            nature:'<i class="fa fa-leaf fa-2x text-primary"></i>',
+            sport:'<i class="fa fa-futbol-o fa-2x text-primary"></i>'
+        };
         for(let question of result){
             let div = $('<a href="#questionsAndComments">');
             div.addClass("question");
@@ -40,7 +48,26 @@ function getThemes() {
                     $('#questionsAndCommentsHeaderBody').text('asam laino');
                 }
             );
-            $('#forumQuestions').append(div);
+            //let table = $('<table  class="table forum table-striped">');
+            let tr = $("<tr>\n");
+            let icon;
+            switch (counter){
+                case 1:icon=typeOfTheme.music;break;
+                case 2:icon=typeOfTheme.game;break;
+                case 3:icon=typeOfTheme.school;break;
+                case 4:icon=typeOfTheme.sport;break;
+                case 0:icon=typeOfTheme.nature;break;
+            }
+            let tdShit = $('<td>');
+            tdShit.append(div);
+            tr.append($(`<td class="text-center">${icon}`))
+                .append(tdShit)
+                .append('<td class="text-center hidden-xs hidden-sm">comments</a></td>\n')
+                .append('<td class="text-center hidden-xs hidden-sm">date</a></td>\n')
+                .append('<td class="hidden-xs hidden-sm">by <a href="#">user that created this question</a></td>\n');
+            //tr.append(div);
+            $('#questionsTable').append(tr);
+            counter++;
         }
     })
 }
